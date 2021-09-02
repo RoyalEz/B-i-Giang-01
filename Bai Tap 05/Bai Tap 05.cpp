1169a33b19499921349ef9d68f64c11cca4b7a64
RoyalEz <89819741+RoyalEz@users.noreply.github.com>
2021-09-02 15:06:39 +0900

    commit
    commit

---
+++ Bai Tap 05/Bai Tap 05.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    system("color fc");
+    cout << "    *     *    *  *     *" << endl;
+    cout << "   * *    **   *  *     *" << endl;
+    cout << "  *   *   * *  *  *     *" << endl;
+    cout << "  *****   *  * *  * * * *" << endl;
+    cout << "  *   *   *   **  *     *" << endl;
+    cout << " *     *  *    *  *     *";
+    return 0;
+}
